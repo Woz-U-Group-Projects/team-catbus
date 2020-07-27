1cfe906b59ca43568c6e5c1f9083ba317523cb64
tist one <tistonela@gmail.com>
2020-07-27 01:10:26 -0700

    new files

---
+++ server-java-mongodb/src/test/java/com/serverjava/mongodb/ServerJavaMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serverjava.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerJavaMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
