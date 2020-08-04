64d96c9e9f83f8071ebe4378e5073551e81f4229
Leonardo Escudero <leonardolearns@gmail.com>
2020-08-03 22:52:45 -0700

    created a layout for the search box

--- client-react/src/components/SearchBox.js
+++ client-react/src/components/SearchBox.js
@@ -3,10 +3,10 @@ import React from 'react';
 const SearchBox = ({ searchChange }) => {
   return (
     <div className='pa2'>
-      <input>
-
-      </input>
-    </div>
+      <input className='pa3 ba b--green bg-lightest-blue'
+        type='search' placeholder='search robots'
+        onChange={searchChange} />
+    </div >
   );
 }
 
