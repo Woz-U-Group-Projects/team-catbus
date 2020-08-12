258fff700d3e271631f1017e751fa0636e0aa0c6
Leonardo Escudero <leonardolearns@gmail.com>
2020-08-11 19:26:41 -0700

    added a route for the task component

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 import Task from "./components/Task";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Task />
-      </div>
+      <Router>
+        <div className="App">
+          <Route path="/" component={Task} exact />
+        </div>
+      </Router>
     );
   }
 }
