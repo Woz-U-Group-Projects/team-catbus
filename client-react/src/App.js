c4318b87beec3c0d1ce7a98e6beedddd4cc61192
Leonardo Escudero <leonardolearns@gmail.com>
2020-08-11 19:14:04 -0700

    changed the function to a class

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Task from "./components/Task";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Task />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Task />
+      </div>
+    );
+  }
 }
 
 export default App;
