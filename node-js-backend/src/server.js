d0afd8b1ff1b1c671e6004635d8a023ed33df975
Leonardo Escudero <leonardolearns@gmail.com>
2020-08-13 17:42:39 -0700

    called body parser

--- node-js-backend/src/server.js
+++ node-js-backend/src/server.js
@@ -3,6 +3,8 @@ import bodyParser from 'body-parser';
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.get('/hello', (req, res) => res.send('Hello'));
 
 app.listen(8000, () => console.log('Listening on port 8000'));
