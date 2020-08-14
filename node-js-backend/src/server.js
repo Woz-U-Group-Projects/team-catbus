6b24a68df792c377ddc0597ca1e1b56ef4b0ef99
Leonardo Escudero <leonardolearns@gmail.com>
2020-08-13 18:57:58 -0700

    added request

--- node-js-backend/src/server.js
+++ node-js-backend/src/server.js
@@ -6,8 +6,22 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/api/GameInfo', (req, res) => {
-  const gameName = req;
+app.get('/api/GameInfo', aync(req, res) => {
+  try {
+    const gameName = req;
+
+    const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true })
+    const db = client.db('gameInfo');
+
+    const gameInfo = await db.collection('godOfWar').findOne({ name: gameName })
+    res.status(200).json(gameInfo);
+
+    client.close();
+  } catch(error) {
+    res.status(500).json({ message: 'Error connecting to db', error });
+  }
+
+
 })
 
 app.get('/hello', (req, res) => res.send('Hello'));
