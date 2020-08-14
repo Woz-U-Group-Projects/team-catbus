1d7b8b88b0f04910fe85e2574434ce32e0d6b700
Leonardo Escudero <leonardolearns@gmail.com>
2020-08-13 17:58:03 -0700

    added a get request to start getting information with MongoDB

--- node-js-backend/src/server.js
+++ node-js-backend/src/server.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.get('/api/GameInfo', (req, res) => {
+  const gameName = req;
+})
+
 app.get('/hello', (req, res) => res.send('Hello'));
 
 app.listen(8000, () => console.log('Listening on port 8000'));
